5b9ade962485728172cf38c5af2877e6a1d7fc8f
Lafifi-24 <hamzalafifi24@gmail.com>
2023-11-25 01:12:04 +0100

    edit api_keys file

--- api_keys.sh
+++ api_keys.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 
+export OPENAI_API_key="<YOUR_OPENAI_API_KEY>"
+export TELEGRAM_API_key="<YOUR_TELEGRAM_API_KEY>"
+export BOOKING_API_key="<YOUR_BOOKING_API_KEY from https://rapidapi.com/tipsters/api/booking-com>"
+
+
