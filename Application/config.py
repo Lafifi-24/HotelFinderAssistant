efc8d8f819f479698e0623cfa2f849556c47a9c9
Lafifi-24 <hamzalafifi24@gmail.com>
2024-01-28 15:53:19 +0100

    the old version stoped working as ngrok start demand auth

--- Application/config.py
+++ Application/config.py
@@ -11,5 +11,7 @@ class Config:
         
         self.BOOKING_API_KEY = os.getenv('BOOKING_API_key')
         if ngrok:
+            Ngrok_auth = os.getenv('Ngrok_API_auth')
+            os.system('ngrok config add-authtoken {}'.format(Ngrok_auth))
             from pyngrok import ngrok
             self.WEBHOOK_URL = ngrok.connect(5000).public_url
--- api_keys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-export OPENAI_API_key="<your openai api key>"
-export TELEGRAM_API_key="<your telegram api key>"
-export BOOKING_API_key="<your booking.com api key from https://rapidapi.com/tipsters/api/booking-com>"
-
-
